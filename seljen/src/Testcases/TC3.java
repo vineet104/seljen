f112d3412872d5faac8e853c44a953c2308771aa
Alka <Alka@192.168.2.12>
2017-08-12 19:57:56 -0400

    Automation proj

---
+++ seljen/src/Testcases/TC3.java
@@ -0,0 +1,22 @@
+package Testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC3 {
+	@Test
+	public void tastcase()
+	{
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Alka\\workspace2\\seljen\\drivers\\chromedriver.exe");
+	ChromeDriver driver = new ChromeDriver();
+	driver.get("http://facebook.com");
+	driver.findElementById("email").sendKeys("Hello");
+	driver.findElement(By.id("pass")).sendKeys("Hello");
+	driver.quit();
+	
+	
+	}
+
+
+}
